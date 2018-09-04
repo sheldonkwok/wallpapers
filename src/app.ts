6c391943ba0e2686741c187dedff92695b2e3201
Sheldon Kwok <me@sheldonk.com>
2018-09-03 22:48:01 -0700

    Load images

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,34 @@
-console.log('hi');
+import manifest from '../dist/sprites/manifest.json';
+
+const { chunkedImages } = manifest;
+const NUM_SPRITES = chunkedImages.length;
+
+let loading = false;
+let page = 1;
+function appendNodes() {
+  if (loading || page === NUM_SPRITES) return;
+  loading = true;
+
+  const parent = document.getElementById('gallery');
+  if (!parent) throw new Error('Could not find parent');
+
+  for (let i = 0; i < chunkedImages[page].length; i++) {
+    const div = document.createElement('div');
+    div.className = 'thumb';
+
+    const style = `background-image:url('/sprites/${page}.jpg'); background-position:-0px -${i * 108}px`;
+    div.setAttribute('style', style);
+
+    parent.appendChild(div);
+  }
+
+  loading = false;
+  page += 1;
+}
+
+window.addEventListener('scroll', _e => {
+  const minWindowRange = document.body.clientHeight - 10;
+  const currPosition = document.documentElement.scrollTop + window.innerHeight;
+
+  if (currPosition >= minWindowRange) appendNodes();
+});
